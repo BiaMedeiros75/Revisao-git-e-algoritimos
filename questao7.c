42a6fd9f4057618103d76429739609480cad7f25
Anna Medeiros <122562181+BiaMedeiros75@users.noreply.github.com>
2025-03-16 15:48:05 -0300

    Create questao7.c

---
+++ questao7.c
@@ -0,0 +1,2 @@
+ /* A manipulação de arquivos em C é feita por meio da biblioteca stdio.h, utilizando a estrutura FILE e funções como fopen, fclose, fprintf, fscanf, fread, fwrite, entre outras. */
+ /* Fgets: Lê uma linha do arquivo (modo texto). Fprintf:	Escreve uma string formatada em um arquivo. Fread:	Lê dados binários de um arquivo. Fwrite:	Escreve dados binários em um arquivo. */
