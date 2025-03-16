38f51067f4375483e8e26cca7c72a87bf99f50d5
Anna Medeiros <122562181+BiaMedeiros75@users.noreply.github.com>
2025-03-16 15:01:05 -0300

    Create questao5.c

---
+++ questao5.c
@@ -0,0 +1,4 @@
+ /* Um vetor é uma estrutura de dados que armazena um conjunto de elementos do mesmo tipo em posições consecutivas da memória, ele permite armazenar e acessar múltiplos valores usando um único nome de variável e índices para referenciar cada elemento. */ 
+ /* Vetor em C: Armazena múltiplos elementos do mesmo tipo */
+ /* Uma string é um vetor de caracteres finalizado com o caractere especial, como C não possui um tipo de dado específico para strings, elas são representadas como arrays de char. */
+ /* String em C: Vetor de char com \0 no final */
