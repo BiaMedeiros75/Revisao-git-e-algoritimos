910310bb5619caa855695c2f46e1dad18c36022d
Anna Medeiros <122562181+BiaMedeiros75@users.noreply.github.com>
2025-03-16 16:06:27 -0300

    Create questao11.c

---
+++ questao11.c
@@ -0,0 +1,5 @@
+ /* A pilha segue o princípio LIFO (Last In, First Out), ou seja, o último elemento inserido é o primeiro a ser removido. A fila segue o princípio FIFO (First In, First Out), ou seja, o primeiro elemento inserido é o primeiro a ser removido.  */
+ /* Vantagens da Fila: Organização de processos e simplicidade. Desvantagens da Fila: Acesso restrito e desempenho em operações aleatórias. */
+ /* Vantagens da Pilha: Simplicidade e facilidade de implementação de algoritmos. Desvantagens da Pilha: Acesso restrito e falta de flexibilidade. */
+ /* Pilha: Recursão, algoritmos de busca e ordenação  e conversão de expressões. Fila: Gestão de processos, sistemas de atendimento e algoritmos de largura em grafos */
+ /* Pilha e Fila São mais restritivas, com regras de acesso e manipulação (LIFO para pilha e FIFO para fila), já a lista são mais flexível, permite acesso e manipulação em qualquer posição e pode ser mais complexa de gerenciar dependendo de sua implementação. */
